d158fa04d6527b112f2cf5e528dfb66f1c4631e1
jonitoh <jordannt94@gmail.com>
2022-08-02 23:22:18 +0200

    fix lint-staged only format:fix

--- services/client/postcss.config.js
+++ services/client/postcss.config.js
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable global-require */
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-preset-env')({
-      browsers: 'last 2 versions',
-    }),
-  ],
+    plugins: [
+        require("autoprefixer"),
+        require("postcss-preset-env")({
+            browsers: "last 2 versions",
+        }),
+    ],
 };
